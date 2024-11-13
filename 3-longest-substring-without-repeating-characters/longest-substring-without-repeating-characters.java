802072fcb94e75bd73bbb1345fc1deeacacf1a48
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-11-13 12:28:47 -0500

    Time: 139 ms (5.39%) | Memory: 45.3 MB (12.04%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int start = 0;
+        HashMap<Character, Integer> ans = new HashMap();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(ans.containsKey(c)) {
+                int charPos = ans.get(c);
+                if(i - start > max) {
+                    max = i - start;
+                }
+                if(charPos >= start) {
+                    start = charPos + 1;
+                }
+            }
+            System.out.println(start);
+            ans.put(c, i);
+        }
+        if(s.length() - start > max) max = s.length() - start;
+        return max;
+    }
+}
