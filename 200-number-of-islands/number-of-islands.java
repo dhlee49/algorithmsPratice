6cd0cc144ebcab4f96750750dcf938353e0afef3
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-11-12 19:58:35 -0500

    Time: 151 ms (5.24%) | Memory: 52.1 MB (17.00%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,28 @@
+class Solution {
+    private char[][] map;
+    private int islands = 0;
+    public int numIslands(char[][] grid) {
+        map = grid;
+
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                if(map[i][j] == '1') {
+                    bfs(i,j);
+                    System.out.println("done");
+                    islands++;
+                }
+            }
+        }
+        return islands;
+    }
+    private void bfs(int x, int y) {
+        //we already checked island, clean up the map
+        map[x][y] = '0';
+        System.out.println(x + " and " + y);
+        if(x - 1 > -1 && map[x-1][y] != '0') bfs(x-1,y);
+        if(y - 1 > -1 && map[x][y-1] != '0') bfs(x,y-1);
+        if(x + 1 < map.length && map[x+1][y] != '0') bfs(x+1,y);
+        if(y + 1 < map[0].length && map[x][y+1] != '0') bfs(x,y+1);
+        return;
+    }
+}
