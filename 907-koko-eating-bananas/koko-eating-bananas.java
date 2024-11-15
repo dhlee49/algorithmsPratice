fd12cb1bec315caf9b394c264b0438a47e9d8fc8
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-11-15 18:14:57 -0500

    Time: 41 ms (7.12%) | Memory: 46.3 MB (6.98%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int min = 0;
+        int max = piles[piles.length - 1];
+        int curr = min + max / 2;
+        int hours = 0;
+        while( min < max) {
+            curr = (min + max + 1)/ 2;
+            if(curr == min || curr == max) return curr;
+            for(int banana : piles) {
+                hours += (banana / curr); 
+                if(banana % curr > 0) hours++;
+            }
+             System.out.println(curr);
+              System.out.println(hours);
+               System.out.println("done");
+            if(hours > h) {
+                min = curr;
+            } else {
+                max = curr;
+            }
+            hours = 0;
+        }
+        return curr;
+    }
+}
