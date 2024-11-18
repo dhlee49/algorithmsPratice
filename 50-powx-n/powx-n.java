602bf49f9e2c2faf4b83b1f8bb0fbf82bc4d2038
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-11-18 02:00:55 -0500

    Time: 0 ms (100.00%) | Memory: 43.2 MB (6.49%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);   
+        if(n == 0) return 1;
+        if(n == Integer.MIN_VALUE) {
+            return x * myPow(1/x, Integer.MAX_VALUE);
+        }
+        if(n < 0){
+            n = -n;
+            x = 1 / x;
+        }
+        double ans = 1;
+        while(n > 0) {
+            if(n % 2 == 1) ans *= x;
+            x *= x;
+            n >>>= 1;
+        }
+        return ans;
     }
+    
 }
