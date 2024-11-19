105914ef354799fcb30bc105f0a2f5c128f577ef
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-11-19 00:16:50 -0500

    Time: 5 ms (46.04%) | Memory: 42.2 MB (10.97%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -2,34 +2,29 @@ class Solution {
     public String reorganizeString(String s) {
         int size = s.length();
         HashMap<Character, Integer> frequency = new HashMap();
-        PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>((a, b) -> b.getValue().compareTo(a.getValue()));
-        int max = 0;
-        for(Character c: s.toCharArray()) {
+        for(char c: s.toCharArray()) {
             frequency.computeIfPresent(c, (k, v) -> v + 1);
             frequency.putIfAbsent(c, 1);
-            if(max < frequency.get(c)) max = frequency.get(c);
         }
-        //Base case. Too many single chars we cant do it
-        if(max > (size + 1) / 2) return "";
-
-        //Build the MaxHeap;
+        PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
         for(Map.Entry<Character, Integer> entry : frequency.entrySet()) {
-            maxHeap.add(entry);
+            maxHeap.offer(entry);
         }
-        //Build string now
-        StringBuffer ans = new StringBuffer();
+        if(maxHeap.peek().getValue() > (size + 1) / 2) return "";
+        StringBuilder ansString = new StringBuilder();
+        Map.Entry<Character, Integer> curr;
         Map.Entry<Character, Integer> prev = null;
-        Map.Entry<Character, Integer> curr = null;
         while(!maxHeap.isEmpty()) {
+            //Add current
             curr = maxHeap.poll();
-            ans.append(curr.getKey());
+            ansString.append(curr.getKey());
             curr.setValue(curr.getValue() - 1);
-            if(prev != null) {
-                maxHeap.offer(prev);
-            }
+            if(prev != null) maxHeap.offer(prev);
             prev = curr;
             if(curr.getValue() == 0) prev = null;
+            
         }
-        return ans.toString();
+        return ansString.toString();
+        
     }
 }
