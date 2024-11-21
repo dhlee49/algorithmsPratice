7f2d809ad3ed1b917d7f0e2a9d802f31662a17e7
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-11-21 00:11:31 -0500

    Time: 12 ms (9.05%) | Memory: 58 MB (5.31%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = (start + end + 1) / 2;
+        if(end == 0) return 0;
+        if(arr[0] > arr[1]) return 0;
+        if(arr[end] > arr[end - 1]) return end;
+        while(start < mid && end > mid) {
+            System.out.println(start + " and " + mid + " and " + end);
+            if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) return mid;
+            if(arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) start = mid;
+            if(arr[mid] < arr[mid - 1] && arr[mid] > arr[mid + 1]) end = mid;
+            mid = (start + end + 1) / 2;
+        }
+        if(mid == start) return start + 1;
+        return end;
+
+    }
+}
