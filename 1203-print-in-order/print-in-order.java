ec649492b4cff6408ea08797f458b31be5f5c836
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-11-22 18:08:21 -0500

    Time: 10 ms (97.62%) | Memory: 42.2 MB (39.33%) - LeetSync

--- 1203-print-in-order/print-in-order.java
+++ 1203-print-in-order/print-in-order.java
@@ -1,34 +1,39 @@
 class Foo {
+    private Object lock;
     private boolean first;
     private boolean second;
     public Foo() {
-        first = false;
-        second = false;
-    }
-
-    public synchronized void first(Runnable printFirst) throws InterruptedException {
-        // printFirst.run() outputs "first". Do not change or remove this line.
-        printFirst.run();
+        lock = new Object();
         first = true;
-        notifyAll();
-
-    }
-
-    public synchronized void second(Runnable printSecond) throws InterruptedException {
-        while(!first) {
-            wait();
-        }
-        // printSecond.run() outputs "second". Do not change or remove this line.
-        printSecond.run();
         second = true;
-        notifyAll();
+        
     }
 
-    public synchronized void third(Runnable printThird) throws InterruptedException {
-        while(!second) {
-            wait();
-        };
+    public void first(Runnable printFirst) throws InterruptedException {
+        synchronized(lock) {
+            printFirst.run();
+            first = false;
+            lock.notifyAll();
+        } 
+        // printFirst.run() outputs "first". Do not change or remove this line.
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        synchronized(lock) {
+            while(first) lock.wait();
+            printSecond.run();
+            second = false;
+            lock.notifyAll();
+        } 
+        // printSecond.run() outputs "second". Do not change or remove this line.
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        
         // printThird.run() outputs "third". Do not change or remove this line.
-        printThird.run();
+        synchronized(lock) {
+            while(second) lock.wait();
+            printThird.run();
+        } 
     }
 }
