c3afdd3073dc0ce64ecfdac3a112ad4e1086e303
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-11-23 23:32:34 -0500

    Time: 711 ms (5.55%) | Memory: 106.3 MB (40.50%) - LeetSync

---
+++ 2151-the-time-when-the-network-becomes-idle/the-time-when-the-network-becomes-idle.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int networkBecomesIdle(int[][] edges, int[] patience) {
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        int[] distances = new int[patience.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> distances[a] - distances[b]);
+        for(int[] edge: edges) {
+            graph.putIfAbsent(edge[0], new LinkedList());
+            graph.putIfAbsent(edge[1], new LinkedList());
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        };
+        Arrays.fill(distances, Integer.MAX_VALUE);
+        distances[0] = 0;
+        //calculate distance from Master -> everything
+        pq.offer(0);
+        HashSet<Integer> visited = new HashSet();
+        while(!pq.isEmpty()) {
+            int curr = pq.poll();
+            if(visited.contains(curr)) continue;
+            visited.add(curr);
+            //curr = current node
+            if(graph.containsKey(curr)) {
+                for(int point : graph.get(curr)) {
+                    if(distances[curr] + 1 < distances[point]) {
+                        distances[point] = distances[curr] + 1;
+                        pq.offer(point);
+                    }
+                }
+            }
+        }
+        int carry = 1;
+        for(int i = 1; i < patience.length; i++) {
+            int curr = distances[i] * 2;
+            if(curr % patience[i] == 0) {
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            distances[i] = curr + (curr / patience[i] - (carry)) * patience[i]  + 1;
+            System.out.println(distances[i]);
+        }
+        return Arrays.stream(distances).max().orElse(-1);
+        
+    }
+}
