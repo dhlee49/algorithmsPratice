803634cadee5d042a817a2cc4f131283740c096d
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-11-24 17:03:37 -0500

    Time: 0 ms (100.00%) | Memory: 40.3 MB (55.39%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,15 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n == 1) return 1;
-        if(n == 2) return 2;
-        int first = 1;
-        int second = 2;
-        int curr = 3;
+        if(n < 3) return n;
+        int[] climb = new int[n + 1];
+        climb[1] = 1;
+        climb[2] = 2;
         for(int i = 3; i <= n; i++) {
-            curr = first + second;
-            first = second;
-            second = curr;
+            climb[i] = climb[i - 1] + climb[i - 2];
         }
-        return curr;
+        return climb[n];
     }
 }
