fd6c79b17b2bc0837f54431af0796407f66be62e
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-12-08 15:01:35 -0500

    Time: 22 ms (92.69%) | Memory: 43.9 MB (10.21%) - LeetSync

--- 1187-print-foobar-alternately/print-foobar-alternately.java
+++ 1187-print-foobar-alternately/print-foobar-alternately.java
@@ -1,40 +1,28 @@
 class FooBar {
     private int n;
-    private Object lock;
-    private boolean first;
-    private boolean second;
+    private final Semaphore fooSem = new Semaphore(1);
+    private final Semaphore barSem = new Semaphore(0);
     public FooBar(int n) {
         this.n = n;
-        lock = new Object();
-        first = false;
-        second = true;
     }
 
     public void foo(Runnable printFoo) throws InterruptedException {
         
         for (int i = 0; i < n; i++) {
-            synchronized(lock) {
-                while(first) lock.wait();
-                printFoo.run();
-                second = false;
-                first = true;
-                lock.notifyAll();
-            }
+            fooSem.acquire(1);
         	// printFoo.run() outputs "foo". Do not change or remove this line.
+        	printFoo.run();
+            barSem.release(1);
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
         
         for (int i = 0; i < n; i++) {
-            
-            synchronized(lock) {
-                while(second) lock.wait();
-                printBar.run();
-                second = true;
-                first = false;
-                lock.notifyAll();
-            }
+            barSem.acquire(1);
+            // printBar.run() outputs "bar". Do not change or remove this line.
+        	printBar.run();
+            fooSem.release(1);
         }
     }
 }
