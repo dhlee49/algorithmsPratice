f32ad5701b669e3606bd91ca2541fcf09c568558
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-12-08 18:01:28 -0500

    Time: 7 ms (30.43%) | Memory: 42.9 MB (11.60%) - LeetSync

--- 1316-fizz-buzz-multithreaded/fizz-buzz-multithreaded.java
+++ 1316-fizz-buzz-multithreaded/fizz-buzz-multithreaded.java
@@ -1,85 +1,66 @@
 class FizzBuzz {
     private int n;
-    private int counter;
-    private ReentrantLock lock;
-    private Condition condition;
+    private final Semaphore fizzSem;
+    private final Semaphore fizzBuzzSem;
+    private final Semaphore buzzSem;
+    private final Semaphore numberSem;
+    private int curr;
     public FizzBuzz(int n) {
         this.n = n;
-        counter = 1;
-        //mutex to control the printing
-        lock  = new ReentrantLock();
-        condition = lock.newCondition();        
+        this.curr = 0;
+        fizzSem = new Semaphore(0);
+        buzzSem = new Semaphore(0);
+        fizzBuzzSem = new Semaphore(0);
+        numberSem = new Semaphore(1);
     }
 
     // printFizz.run() outputs "fizz".
     public void fizz(Runnable printFizz) throws InterruptedException {
-        while(true) {
-            lock.lock();
-            try {
-                while(counter > n || counter % 3 != 0 || counter % 5 == 0) {
-                    if(counter > n) return;
-                    condition.await();
-                }
-                printFizz.run();
-                counter++;
-                condition.signalAll();
-            } finally {
-                lock.unlock();
-            }
+        while(curr <= n) {
+            fizzSem.acquire();
+            if(curr <= n) printFizz.run();
+            numberSem.release(1);
         }
     }
 
     // printBuzz.run() outputs "buzz".
     public void buzz(Runnable printBuzz) throws InterruptedException {
-        while(true) {
-            lock.lock();
-            try {
-                while(counter > n || counter % 5 != 0 || counter % 3 == 0) {
-                    if(counter > n) return;
-                    condition.await();
-                }
-                printBuzz.run();
-                counter++;
-                condition.signalAll();
-            } finally {
-                lock.unlock();
-            }
+        while(curr <= n) {
+            buzzSem.acquire();
+            if(curr <= n) printBuzz.run();
+            numberSem.release(1);
         }
     }
 
     // printFizzBuzz.run() outputs "fizzbuzz".
     public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
-        while(true) {
-            lock.lock();
-            try {
-                while(counter > n || counter % 5 != 0 || counter % 3 != 0) {
-                    if(counter > n) return;
-                    condition.await();
-                }
-                printFizzBuzz.run();
-                counter++;
-                condition.signalAll();
-            } finally {
-                lock.unlock();
-            }
+        while(curr <= n) {
+            fizzBuzzSem.acquire(1);
+            if(curr <= n) printFizzBuzz.run();
+            numberSem.release(1);
         }
     }
 
     // printNumber.accept(x) outputs "x", where x is an integer.
     public void number(IntConsumer printNumber) throws InterruptedException {
-        while(true) {
-            lock.lock();
-            try {
-                while(counter > n || counter % 5 == 0 || counter % 3 == 0) {
-                    if(counter > n) return;
-                    condition.await();
-                }
-                printNumber.accept(counter);
-                counter++;
-                condition.signalAll();
-            } finally {
-                lock.unlock();
-            }
-        }
+        while(curr <= n) {
+            numberSem.acquire(1);
+            curr++;
+            if(curr > n) {
+                fizzSem.release(1);
+                buzzSem.release(1);
+                fizzBuzzSem.release(1);
+                numberSem.release(1);
+            } else if(curr % 5 == 0 && curr % 3 == 0) {
+                fizzBuzzSem.release(1);
+            } else if(curr % 5 == 0) {
+                buzzSem.release(1);
+            } else if(curr % 3 == 0) {
+                fizzSem.release(1);
+            } else {
+                printNumber.accept(curr);
+                numberSem.release(1);
+            } 
+        } 
     }
 }
