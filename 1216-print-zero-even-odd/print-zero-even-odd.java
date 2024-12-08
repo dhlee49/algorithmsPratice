9ca3e492d172050486b355354de27686eac80532
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-12-08 15:22:16 -0500

    Time: 6 ms (100.00%) | Memory: 42.2 MB (31.72%) - LeetSync

--- 1216-print-zero-even-odd/print-zero-even-odd.java
+++ 1216-print-zero-even-odd/print-zero-even-odd.java
@@ -1,50 +1,47 @@
 class ZeroEvenOdd {
     private int n;
-    private boolean printZero;
-    private boolean printOne;
-    private boolean printTwo;
-    
+    private final Semaphore zeroSem = new Semaphore(1);
+    private final Semaphore evenSem = new Semaphore(0);
+    private final Semaphore oddSem = new Semaphore(0);
+    private int curr;
     public ZeroEvenOdd(int n) {
         this.n = n;
-        printZero = true;
-        printOne = false;
-        printTwo = false;
+        this.curr = 1;
     }
 
     // printNumber.accept(x) outputs "x", where x is an integer.
-    public synchronized void zero(IntConsumer printNumber) throws InterruptedException {
-        for(int i = 1; i <= n; i ++) {
-            while(!printZero) wait();
-            printNumber.accept(0);
-            printZero = false;
-            if(i % 2 == 1) {
-                printOne = true;
-                printTwo = false;
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        while(curr <= n) {
+            zeroSem.acquire();
+            if(curr <= n) {
+                printNumber.accept(0);
+                if(curr % 2 == 0) {
+                    evenSem.release(1);
+                } else {
+                    oddSem.release(1);
+                }
             } else {
-                printOne = false;
-                printTwo = true;
+                oddSem.release(1);
+                evenSem.release(1);
             }
-            notifyAll();
         }
     }
 
-    public synchronized void odd(IntConsumer printNumber) throws InterruptedException {
-        for(int i = 1; i <= n; i += 2) {
-            while(!printOne) wait();
-            printNumber.accept(i);
-            printOne = false;
-            printZero = true;
-            notifyAll();
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        while(curr <= n) {
+            evenSem.acquire(1);
+            if(curr <= n) printNumber.accept(curr);
+            curr++;
+            zeroSem.release(1);
         }
     }
 
-    public synchronized void even(IntConsumer printNumber) throws InterruptedException {
-        for(int i = 2; i <= n; i += 2) {
-            while(!printTwo) wait();
-            printNumber.accept(i);
-            printTwo = false;
-            printZero = true;
-            notifyAll();
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        while(curr <= n) {
+            oddSem.acquire(1);
+            if(curr <= n) printNumber.accept(curr);
+            curr++;
+            zeroSem.release(1);
         }
     }
 }
