53aef7fdc869a7eaf0da2a8facd9f338d21b4408
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-12-09 00:10:39 -0500

    Time: 39 ms (87.01%) | Memory: 99.1 MB (75.00%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
@@ -1,16 +1,16 @@
 class Solution {
     public int maxTwoEvents(int[][] events) {
         Arrays.sort(events, (a,b) -> a[0] - b[0]);
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        int ans = 0;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         int curr = 0;
+        int max = 0;
         for(int[] event : events) {
-            while(!minHeap.isEmpty() && minHeap.peek()[1] < event[0]) {
+            while(!minHeap.isEmpty() && event[0] > minHeap.peek()[1]) {
                 curr = Math.max(curr, minHeap.poll()[2]);
             }
-            ans = Math.max(ans, event[2] + curr);
+            max = Math.max(max, curr + event[2]);
             minHeap.offer(event);
         }
-        return ans;
+        return max;
     }
 }
