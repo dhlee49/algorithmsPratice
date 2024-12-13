20471f2abed9b97724d549308a92d949bacc15d8
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-12-12 23:39:58 -0500

    Time: 104 ms (96.57%) | Memory: 60.9 MB (32.87%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -1,22 +1,22 @@
 class Solution {
     public long findScore(int[] nums) {
         int[][] index = new int[nums.length][2];
+        boolean[] marked = new boolean[nums.length];
         for(int i = 0; i < nums.length; i++) {
             index[i][0] = nums[i];
             index[i][1] = i;
         }
         Arrays.sort(index, (a,b) -> a[0] - b[0]);
         long sum = 0;
-        Set<Integer> indexes = new HashSet();
         int idx = 0;
         while(idx < nums.length) {
             int[] curr = index[idx];
             idx++;
-            if(indexes.contains(curr[1])) continue;
-            indexes.add(curr[1]);
+            if(marked[curr[1]]) continue;
+            marked[curr[1]] = true;
             sum += curr[0];
-            if(curr[1] != nums.length - 1) indexes.add(curr[1] + 1);
-            if(curr[1] != 0) indexes.add(curr[1] - 1);
+            if(curr[1] != nums.length - 1) marked[curr[1] + 1] = true;
+            if(curr[1] != 0) marked[curr[1] - 1] = true;
         }
         return sum;
     }
