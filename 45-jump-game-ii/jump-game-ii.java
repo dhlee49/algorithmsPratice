489cb1a173acf4252baa681c05bfa981ec4c813e
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-12-13 23:48:08 -0500

    Time: 38 ms (17.65%) | Memory: 45.3 MB (31.90%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,13 +1,14 @@
 class Solution {
     public int jump(int[] nums) {
-        int count = 0, idx = 0, max = 0;
-        for(int i = 0; i < nums.length - 1; i++) {
-            max = Math.max(max, i + nums[i]);
-            if(i == idx) {
-                count++;
-                idx = max;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 1; j <= nums[i]; j++) {
+                if(i + j >= nums.length) break;
+                dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
             }
         }
-        return count; 
+        return dp[nums.length - 1];
     }
 }
