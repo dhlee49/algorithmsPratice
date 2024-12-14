624f98936d8e5a313daa401227609eaee9345e95
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-12-13 22:41:35 -0500

    Time: 0 ms (100.00%) | Memory: 57.4 MB (37.22%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -3,8 +3,18 @@ class Solution {
         Queue<Integer> queue = new LinkedList();
         int size = nums.length;
         k = k % size;
-        for(int i = size - k; i < size; i++) queue.offer(nums[i]);
-        for(int i = size - k - 1; i >= 0; i--) nums[i + k] = nums[i];
-        for(int i = 0; i < k; i++) nums[i] = queue.poll();
+        reverse(nums, 0, size - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, size - 1);
+    }
+    private void reverse(int[] nums, int start, int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
+
