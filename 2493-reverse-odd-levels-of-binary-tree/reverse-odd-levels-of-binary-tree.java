a8faf4579bb9b460e30cb2030f1331a33a2d1765
Dong Hoon Lee <l.donghoon49@gmail.com>
2024-12-20 01:54:27 -0500

    Time: 252 ms (5.35%) | Memory: 50.2 MB (19.56%) - LeetSync

---
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode reverseOddLevels(TreeNode root) {
+        int level = 0;
+        Deque<TreeNode> dq = new LinkedList();
+        dq.offerLast(root);
+        while(!dq.isEmpty()) {
+            level++;
+            int size = dq.size();
+            List<Integer> currLevel = new LinkedList();
+            Queue<TreeNode> temp = new LinkedList();
+            for(int i = 0; i < size; i++) {
+                TreeNode curr = dq.poll();
+                if(curr.left == null) break;
+                temp.offer(curr.left);
+                temp.offer(curr.right);
+                dq.offer(curr.left);
+                dq.offer(curr.right);
+                currLevel.add(curr.left.val);
+                currLevel.add(curr.right.val);
+            }
+            if(level % 2 == 1) {
+                Collections.reverse(currLevel);
+                for(int i = 0; i < currLevel.size(); i++) {
+                    TreeNode curr = temp.poll();
+                    curr.val = currLevel.get(i);
+                }
+            }
+        }
+        return root;
+    }
+    private void swapChild(TreeNode node1, TreeNode node2) {
+        TreeNode tempLeft = node1.left;
+        TreeNode tempRight = node1.right;
+        if(tempLeft == null) return;
+        node1.left = node2.left;
+        node1.right = node2.right;
+        node2.left = tempLeft;
+        node2.right = tempRight;
+    }
+}
