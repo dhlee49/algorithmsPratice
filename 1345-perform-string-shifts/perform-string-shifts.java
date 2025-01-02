a800a0a39084280d93edec4227af1cd1c1faa28d
Dong Hoon Lee <l.donghoon49@gmail.com>
2025-01-01 22:31:01 -0500

    Time: 0 ms (100.00%) | Memory: 41.9 MB (28.43%) - LeetSync

---
+++ 1345-perform-string-shifts/perform-string-shifts.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String stringShift(String s, int[][] shift) {
+        int totalShift = 0;
+        for(int[] curr : shift) {
+            totalShift += (curr[0] == 1 ? -1 : 1) * (curr[1] % s.length());
+            totalShift %= s.length();
+        }
+        if(totalShift < 0) totalShift = s.length() + totalShift;
+        return s.substring(totalShift, s.length()) + s.substring(0, totalShift);
+    }
+}
