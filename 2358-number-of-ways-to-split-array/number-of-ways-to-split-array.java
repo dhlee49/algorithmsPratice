dffe00841be3dc6db316af93376d1fbf5dc0c082
Dong Hoon Lee <l.donghoon49@gmail.com>
2025-01-03 09:30:05 -0500

    Time: 3 ms (59.69%) | Memory: 58.4 MB (60.21%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        long sum = Arrays.stream(nums).mapToLong(i -> i).sum();
+        long sum = 0;
+        for(int num : nums) sum += num;
         long prefixSum = 0;
         int cnt = 0;
         for(int i = 0; i < nums.length - 1; i++) {
