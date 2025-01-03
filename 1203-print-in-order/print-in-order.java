d37fbcebef28f8a1b2b31d7d72a0ce390151a108
Dong Hoon Lee <l.donghoon49@gmail.com>
2025-01-03 10:24:16 -0500

    Time: 12 ms (57.11%) | Memory: 42.8 MB (8.29%) - LeetSync

--- 1203-print-in-order/print-in-order.java
+++ 1203-print-in-order/print-in-order.java
@@ -1,26 +1,31 @@
 class Foo {
-    private final Semaphore second = new Semaphore(0);
-    private final Semaphore third = new Semaphore(0);
+    private Semaphore first;
+    private Semaphore second;
+    
     public Foo() {
+        first = new Semaphore(0);
+        second = new Semaphore(0);
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
+        
         // printFirst.run() outputs "first". Do not change or remove this line.
         printFirst.run();
-        second.release(1);
+        first.release(1);
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        second.acquire();
-        // printSecond.run() outputs "second". Do not change or remove this line.
-        printSecond.run();
-        third.release(1);
         
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        first.acquire(1);
+        printSecond.run();
+        second.release(1);
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        third.acquire(1);
+        
         // printThird.run() outputs "third". Do not change or remove this line.
+        second.acquire(1);
         printThird.run();
     }
 }
