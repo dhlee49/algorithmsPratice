f44a1f4f420722b8d86d68af4965d0be20f30d3e
Dong Hoon Lee <l.donghoon49@gmail.com>
2025-01-07 17:33:41 -0500

    Time: 586 ms (4.06%) | Memory: 57.4 MB (97.68%) - LeetSync

---
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+        long prefixSum = 0;
+        long totalSum = 0;
+        long cnt = 0;
+        Queue<Integer> que = new LinkedList();
+        for(int i = 0; i < nums.length; i++) {
+            que.offer(nums[i]);
+            prefixSum += nums[i];
+            totalSum = prefixSum * que.size();
+            while(totalSum >= k) {
+                System.out.println(i);
+                long prev = que.poll();
+                prefixSum -= prev;
+                totalSum = prefixSum * que.size();
+            }
+            cnt += que.size();
+        }
+        return cnt;   
+    }
+}
