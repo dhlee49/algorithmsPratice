8a6431608a93e547db2ee77239ff79398036193d
Dong Hoon Lee <l.donghoon49@gmail.com>
2025-01-09 09:57:11 -0500

    Time: 7 ms (62.67%) | Memory: 41 MB (49.33%) - LeetSync

--- 465-optimal-account-balancing/optimal-account-balancing.java
+++ 465-optimal-account-balancing/optimal-account-balancing.java
@@ -1,34 +1,30 @@
 class Solution {
     public int minTransfers(int[][] transactions) {
-        Map<Integer, Integer> history = new HashMap();
+        Map<Integer, Integer> entries = new HashMap();
         for(int[] transaction : transactions) {
-            history.putIfAbsent(transaction[0], 0);
-            history.putIfAbsent(transaction[1], 0);
-            history.computeIfPresent(transaction[0], (k, v) -> v - transaction[2]);
-            history.computeIfPresent(transaction[1], (k, v) -> v + transaction[2]);
+            entries.putIfAbsent(transaction[0], 0);
+            entries.putIfAbsent(transaction[1], 0);
+            entries.computeIfPresent(transaction[0], (k,v) -> v - transaction[2]);
+            entries.computeIfPresent(transaction[1], (k,v) -> v + transaction[2]);
         }
-        PriorityQueue<Integer> minHeap = new PriorityQueue();
-        int cnt = 0;
-        int[] entries = new int[history.size()];
+        int[] values = new int[entries.size()];
         int i = 0;
-        for(Map.Entry<Integer, Integer> status : history.entrySet()) {
-            //System.out.println(" key " + status.getKey() + " value " + status.getValue());
-            entries[i++] = status.getValue();
+        for(Integer key : entries.keySet()) {
+            values[i++] = entries.get(key);
         }
-        return getMinCount(0, entries);
+        return dfs(0, values);
     }
-    private int getMinCount(int idx, int[] values) {
-        if(idx == values.length) return 0; //No more action required
-        if(values[idx] == 0) return getMinCount(idx + 1, values);
-        int count = values.length * values.length + 1;
+    private int dfs(int idx, int[] values) {
+        if(idx == values.length) return 0;
+        //This account cannot perform any transaction
+        if(values[idx] == 0) return dfs(idx + 1, values);
+        int max = Integer.MAX_VALUE;
         for(int i = idx + 1; i < values.length; i++) {
-            //BaseCase. no transfer needed because
-            // 1. Both are positive, negative, or 1 is 0
             if(values[idx] * values[i] >= 0) continue;
             values[i] += values[idx];
-            count = Math.min(count, 1 + getMinCount(idx + 1, values));
+            max = Math.min(max, 1 + dfs(idx + 1, values));
             values[i] -= values[idx];
         }
-        return count;
+        return max;
     }
 }
