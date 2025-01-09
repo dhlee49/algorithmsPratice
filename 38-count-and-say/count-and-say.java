41e416d0e70ce192bde61358238286240fe10e9d
Dong Hoon Lee <l.donghoon49@gmail.com>
2025-01-08 20:17:14 -0500

    Time: 1 ms (99.73%) | Memory: 41.2 MB (83.24%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,39 +1,31 @@
 class Solution {
     public String countAndSay(int n) {
-        return countToString(getRecursiveMap(n));
-    }
-    private List<int[]> getRecursiveMap(int n) {
-        if(n == 1) {
-            return null;
+        String ans = "1";
+        for(int i = 1; i < n; i ++) {
+            ans = helper(ans);
         }
-        String curr = countToString(getRecursiveMap(n - 1));
+        return ans;
+    }
+    private String helper(String start) {
         char prev = '\0';
-        int cnt = 0;
-        List<int[]> result = new LinkedList();
-        for(char c: curr.toCharArray()) {
+        int cnt = 1;
+        StringBuilder result = new StringBuilder();
+        for(char c : start.toCharArray()) {
             if(prev == '\0') {
                 prev = c;
-                cnt = 1;
                 continue;
             }
-            if(c != prev) {
-                result.add(new int[] { cnt, prev - '0' });
+            if(prev != c) {
+                result.append(cnt);
+                result.append(prev);
                 prev = c;
                 cnt = 1;
             } else {
                 cnt++;
             }
         }
-        result.add(new int[] { cnt, prev - '0'});
-        return result;
-    }
-    private String countToString(List<int[]> curr) {
-        if(curr == null) return "1";
-        StringBuilder sb = new StringBuilder();
-        for(int[] x: curr) {
-            sb.append(x[0]);
-            sb.append(x[1]);
-        }
-        return sb.toString();
+        result.append(cnt);
+        result.append(prev);
+        return result.toString();
     }
 }
