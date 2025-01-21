69481f01b6b45a4c95f960f0c6163d24574e9545
Dong Hoon Lee <l.donghoon49@gmail.com>
2025-01-21 10:48:56 -0500

    Time: 47 ms (42.93%) | Memory: 55 MB (96.70%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -1,24 +1,24 @@
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        ArrayList<int[]> entries = new ArrayList();
-        for(int i = 0; i < startTime.length; i++) {
-            entries.add(new int[] {startTime[i], endTime[i], profit[i]});
+        int[][] entries = new int[startTime.length][3];
+        for(int i = 0; i < entries.length; i++) {
+            entries[i][0] = startTime[i];
+            entries[i][1] = endTime[i];
+            entries[i][2] = profit[i];
         }
-        //Sort it by endTime in asecnding order
-        entries.sort((a, b) -> (a[1] - b[1]));
-
-
-        TreeMap<Integer, Integer> sortedEntries = new TreeMap<>((a, b) -> a - b); 
-        sortedEntries.put(0, 0);
-        int prevMax = -1;
+        Arrays.sort(entries, (a, b) -> a[1] - b[1]);
+        //EndTime, Profit
+        TreeMap<Integer, Integer> sortedEndTime = new TreeMap();
+        sortedEndTime.put(0, 0);
+        int currVal = 0;
+        int curMax = 0;
         for(int[] entry : entries) {
-            Map.Entry<Integer, Integer> beforeStart = sortedEntries.floorEntry(entry[0]);
-            Map.Entry<Integer, Integer> beforeEnd = sortedEntries.floorEntry(entry[1]);
-            if(beforeStart.getValue() + entry[2] > beforeEnd.getValue()){
-                sortedEntries.put(entry[1], beforeStart.getValue() + entry[2]);
+            currVal = sortedEndTime.floorEntry(entry[0]).getValue();
+            if(currVal + entry[2] > curMax) {
+                curMax = currVal + entry[2];
+                sortedEndTime.put(entry[1], curMax);
             }
-
         }
-        return sortedEntries.lastEntry().getValue();
+        return sortedEndTime.lastEntry().getValue();
     }
 }
