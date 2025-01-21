2cf12b19c67974ffbec968af8415c9519bf12943
Dong Hoon Lee <l.donghoon49@gmail.com>
2025-01-20 21:18:22 -0500

    Time: 0 ms (100.00%) | Memory: 42.3 MB (83.27%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,31 +1,29 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left = 0;
-        int right = nums.length;
-        int size = nums.length - 1;
-        int mid = (left + right) / 2;
-        int lVal = 0;
-        int rVal = 0;
-        if(size == 0) return 0;
-        while(left < right) {
-            if(mid == 0) {
-                lVal = Integer.MIN_VALUE;
-                rVal = nums[mid + 1];
-            } else if(mid == size) {
-                lVal = nums[mid - 1];
-                rVal = Integer.MIN_VALUE;
-            } else {
-                lVal = nums[mid - 1];
-                rVal = nums[mid + 1];
+        if(nums.length == 1) return 0;
+        if(nums.length == 2) return nums[0] > nums[1] ? 0 : 1;
+        int l = 0;
+        int r = nums.length - 1;
+        int idx = l + (r - l) / 2;
+        while(r >= l) {
+            idx = l + (r - l) / 2;
+            if(idx == 0) {
+                if(nums[idx] > nums[idx + 1]) return idx;
+                l = idx + 1;
+                continue;
             }
-            if(lVal < nums[mid]) {
-                if(rVal < nums[mid]) return mid;
-                left = mid + 1;
-            } else {
-                right = mid;
+            if(idx == nums.length - 1) {
+                if(nums[idx] > nums[idx - 1]) return idx;
+                r = idx - 1;
+                continue;
             }
-            mid = (left + right) / 2;
+            if(nums[idx] > nums[idx - 1] && nums[idx] > nums[idx + 1]) return idx;
+            if(nums[idx] > nums[idx - 1]) {
+                l = idx + 1;
+            } else {
+                r = idx - 1;
+            }
         }
-        return mid;
+        return idx;
     }
 }
