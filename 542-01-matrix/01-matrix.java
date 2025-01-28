9bbf61c63a3e8064e45d8eb6e5cd6ba8546dcf00
Dong Hoon Lee <l.donghoon49@gmail.com>
2025-01-27 23:13:31 -0500

    Time: 752 ms (5.01%) | Memory: 47.1 MB (77.75%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,19 +1,28 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        Queue<int[]> bfsQueue = new LinkedList();
-        for(int i = 0; i < mat.length; i++) {
-            for(int j = 0; j < mat[0].length; j++) {
-                if(mat[i][j] == 0) bfsQueue.offer(new int[] {i, j});
-                if(mat[i][j] == 1) mat[i][j] = -1;
+        int[][] directions = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int xLen = mat.length;
+        int yLen = mat[0].length;
+        Queue<int[]> bfsQueue = new LinkedList<>(); 
+        for(int i = 0; i < xLen; i++) {
+            for(int j = 0; j < yLen; j++) {
+                if(mat[i][j] == 0) {
+                    bfsQueue.offer(new int[] {i, j});
+                } else {
+                    mat[i][j] = -1;
+                }
             }
         }
-        int[][] directions = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         while(!bfsQueue.isEmpty()) {
             int[] curr = bfsQueue.poll();
+            int x = curr[0];
+            int y = curr[1];
+            System.out.println(x + "  & " + y);
+            System.out.println(mat[x][y]);
             for(int[] direction : directions) {
-                int x = curr[0] + direction[0];
-                int y = curr[1] + direction[1];
-                if(x < 0 || x >= mat.length || y < 0 || y >= mat[0].length || mat[x][y] > -1) continue;
+                x = curr[0] + direction[0];
+                y = curr[1] + direction[1];
+                if(x < 0 || x >= xLen || y < 0 || y >= yLen || mat[x][y] == 0 || mat[x][y] > 0) continue;
                 mat[x][y] = mat[curr[0]][curr[1]] + 1;
                 bfsQueue.offer(new int[] {x, y});
             }
