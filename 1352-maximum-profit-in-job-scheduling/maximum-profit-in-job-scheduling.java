a531eb97c0a4c3a283177e82609beffc4368db9b
Dong Hoon Lee <l.donghoon49@gmail.com>
2025-02-13 11:47:48 -0500

    Time: 708 ms (11.72%) | Memory: 56.1 MB (70.86%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -1,24 +1,43 @@
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        int[][] entries = new int[startTime.length][3];
-        for(int i = 0; i < entries.length; i++) {
-            entries[i][0] = startTime[i];
-            entries[i][1] = endTime[i];
-            entries[i][2] = profit[i];
+        int[][] combinedEntries = new int[startTime.length][3];
+        for(int i = 0; i < startTime.length; i++) {
+            combinedEntries[i][0] = startTime[i];
+            combinedEntries[i][1] = endTime[i];
+            combinedEntries[i][2] = profit[i];
         }
-        Arrays.sort(entries, (a, b) -> a[1] - b[1]);
-        //EndTime, Profit
-        TreeMap<Integer, Integer> sortedEndTime = new TreeMap();
-        sortedEndTime.put(0, 0);
-        int currVal = 0;
-        int curMax = 0;
-        for(int[] entry : entries) {
-            currVal = sortedEndTime.floorEntry(entry[0]).getValue();
-            if(currVal + entry[2] > curMax) {
-                curMax = currVal + entry[2];
-                sortedEndTime.put(entry[1], curMax);
+        Arrays.sort(combinedEntries, (a, b) -> a[1] - b[1]);
+        ArrayList<int[]> entryList = new ArrayList<>();
+        entryList.add(new int[] {0, 0});
+        int max = 0;
+        for(int[] entry : combinedEntries) {
+            //Search for closest end time to this
+            int idx = binarySearch(entry[0], entryList);
+            int currTotal = entryList.get(idx)[1] + entry[2];
+            if(max < currTotal) {
+                max = currTotal;
+                entryList.add(new int[] {entry[1], currTotal});
             }
         }
-        return sortedEndTime.lastEntry().getValue();
+        return entryList.get(entryList.size() - 1)[1];
+    }
+    //Given startTime, list of endTime + price return closest end time to startTime
+    private int binarySearch(int startTime, ArrayList<int[]> entryList) {
+        //There is 0,0 as base entry so you are guranteed to find something
+        int l = 0;
+        int r = entryList.size();
+        System.out.println("For start time : "  + startTime);
+        while(r > l) {
+            int mid = l + (r - l) / 2;
+            int[] curr = entryList.get(mid);
+            if(curr[0] > startTime) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        System.out.println("result : " + l);
+        return l - 1;
+        
     }
 }
